9b6a15f2a9f2401dc8aa14ed3c2f25de847b6405
Yuk Lam <yukchenglam@gmail.com>
2017-12-08 16:31:37 +0100

    Edit, update and delete dinosaurs

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -9,11 +9,11 @@ class DinosaursController < ApplicationController
   end
 
   def new
-      @dinosaur = Dinosaur.new
+    @dinosaur = Dinosaur.new
   end
 
   def create
-    dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
+  #  dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
 
     @dinosaur = Dinosaur.new(dinosaur_params)
 
@@ -22,6 +22,36 @@ class DinosaursController < ApplicationController
     else
        render 'new'
     end
+  end
+
+  def edit
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def update
+    @dinosaur = Dinosaur.find(params[:id])
+
+  #  dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
+
+    if @dinosaur.update_attributes(dinosaur_params)
+      redirect_to @dinosaur
+    else
+      render 'edit'
+    end
+  end
+
+    def destroy
+      @dinosaur = Dinosaur.find(params[:id])
+
+      @dinosaur.destroy
+
+      redirect_to dinosaurs_path
+    end
+
+private
+
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :age, :image_url)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
 
     root "dinosaurs#index"
 
-    get "dinosaurs" => "dinosaurs#index"
-    get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
-    get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+    resources :dinosaurs
 
-      post "dinosaurs" => "dinosaurs#create"
-      
+  #  get "dinosaurs" => "dinosaurs#index"
+  #  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  #  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+  #  post "dinosaurs" => "dinosaurs#create"
+  #  get 'dinosaurs/:id/edit' => 'dinosaurs#edit', as: :edit_dinosaur
+
+  #  patch "dinosaurs/:id" => "dinosaurs#update"
+
+  #  delete "dinosaurs/:id" => "dinosaurs#destroy"
 end
