c2d785db9b3eef13d0393a07dfd5dab54bc5d38b
Yuk Lam <yukchenglam@gmail.com>
2017-12-08 13:22:40 +0100

    Create dinosaur model

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ValleysOfDinosaurs
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    # Some code and comments are here already. Just leave those here, and add these lines
+    console do
+      ActiveRecord::Base.connection
+    end
+  end
+end
