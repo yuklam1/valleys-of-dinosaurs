d625025bb9139b18fc86db57d6f324de523091e9
Yuk Lam <yukchenglam@gmail.com>
2017-12-08 16:18:02 +0100

    Create show and new dinosaur pages

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -4,4 +4,24 @@ class DinosaursController < ApplicationController
     @dinosaurs = Dinosaur.all
   end
 
+  def show
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def new
+      @dinosaur = Dinosaur.new
+  end
+
+  def create
+    dinosaur_params = params.require(:dinosaur).permit(:name, :age, :image_url)
+
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+    if @dinosaur.save
+       redirect_to @dinosaur
+    else
+       render 'new'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  
+
     root "dinosaurs#index"
 
     get "dinosaurs" => "dinosaurs#index"
+    get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+    get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+
+      post "dinosaurs" => "dinosaurs#create"
+      
 end
